76077b98a931bec7d94abd62a37314115cdbc671
Leo Venceslau <leonardocvenceslau@gmail.com>
2024-05-10 16:32:04 -0400

    lazy setup

---
+++ lua/config/lazy.lua
@@ -0,0 +1,22 @@
+local lazyPath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
+
+if not (vim.uv or vim.loop).fs_stat(lazyPath) then
+	vim.fn.system({
+		'git',
+		'clone',
+		'--filter=blob:none',
+		'https://github.com/folke/lazy.nvim.git',
+		'--branch=stable',
+		lazyPath,
+	})
+end
+
+vim.opt.rtp:prepend(lazyPath)
+
+require('lazy').setup('plugin', {
+	ui = { border = 'rounded' },
+	change_detection = {
+		enabled = true,
+		notify = false,
+	}
+})
