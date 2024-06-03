8eb295038aaf724d09faaf0bf83049d1581d1957
Leo <135278016+ROCKET-P13@users.noreply.github.com>
2024-06-03 06:14:08 -0400

    feat: auto-completion initial setup (cmp) (#8)
    * cmp setup
    * restruct
    * return
    * cleanup

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -6,19 +6,18 @@ vim.opt.wrap = false
 vim.opt.cursorline = true
 vim.opt.fixendofline = false
 vim.opt.ignorecase = true
-vim.opt.smartcase = true 
-vim.opt.splitbelow = true 
-vim.opt.splitright = true 
-vim.opt.swapfile = false 
-vim.opt.termguicolors = true 
-vim.opt.timeoutlen = 1000 
-vim.opt.undofile = true 
-vim.opt.updatetime = 100 
+vim.opt.smartcase = true
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+vim.opt.swapfile = false
+vim.opt.termguicolors = true
+vim.opt.timeoutlen = 1000
+vim.opt.undofile = true
+vim.opt.updatetime = 100
 vim.opt.laststatus = 3
 vim.opt.showcmd = false
 vim.opt.ruler = false
 vim.opt.title = false
-vim.opt.completeopt = { "menu", "menuone", "noselect" } 
 vim.opt.fillchars = vim.opt.fillchars + "eob: "
 vim.opt.fillchars:append({
 	stl = " ",
@@ -36,4 +35,3 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 		vim.highlight.on_yank()
 	end,
 })
-
---
+++ lua/plugins/cmp.lua
@@ -0,0 +1,44 @@
+local M = {
+	"hrsh7th/nvim-cmp",
+	event = "InsertEnter",
+	dependencies = {
+		"hrsh7th/cmp-buffer",
+		"hrsh7th/cmp-path",
+		"L3MON4D3/LuaSnip",
+		"saadparwaiz1/cmp_luasnip",
+		"onsails/lspkind.nvim",
+	},
+}
+
+function M.config()
+	local cmp = require("cmp")
+	local luasnip = require("luasnip")
+	local lspkind = require("lspkind")
+
+	cmp.setup({
+		snippet = {
+			expand = function(args)
+				luasnip.lsp_expand(args.body)
+			end,
+		},
+		mapping = cmp.mapping.preset.insert({
+			["<C-b>"] = cmp.mapping.scroll_docs(-4),
+			["<C-f>"] = cmp.mapping.scroll_docs(4),
+			["<CR>"] = cmp.mapping.confirm({ select = true }),
+		}),
+		sources = cmp.config.sources({
+			{ name = "nvim_lsp" },
+			{ name = "buffer" },
+			{ name = "path" },
+		}),
+		formatting = {
+			format = lspkind.cmp_format({
+				maxwidth = 50,
+				ellipsis_char = "...",
+			}),
+		},
+		completion = { completeopt = "menu,menuone,noinsert" },
+	})
+end
+
+return M
