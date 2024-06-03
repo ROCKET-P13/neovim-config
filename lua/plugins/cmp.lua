c478b5cf350fe41b0c8e0b0505d6b6ce057739fb
Leo Venceslau <Leonardo.Venceslau@rfsmart.com>
2024-06-03 09:54:14 -0400

    lsp update

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -25,11 +25,11 @@ function M.config()
 		mapping = cmp.mapping.preset.insert({
 			["<C-b>"] = cmp.mapping.scroll_docs(-4),
 			["<C-f>"] = cmp.mapping.scroll_docs(4),
-			["<CR>"] = cmp.mapping.confirm({ select = true }),
+			["<CR>"] = cmp.mapping.confirm({ select = false }),
 		}),
 		sources = cmp.config.sources({
-			{ name = "nvim_lsp" },
 			{ name = "buffer" },
+			{ name = "nvim_lsp" },
 			{ name = "path" },
 		}),
 		formatting = {
@@ -39,6 +39,9 @@ function M.config()
 			}),
 		},
 		completion = { completeopt = "menu,menuone,noinsert" },
+		performance = {
+			max_view_entries = 12,
+		},
 	})
 end
 
