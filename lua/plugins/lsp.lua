228ba7cd691977b7306a301f5b54c5a3d9ebddd6
Leo Venceslau <Leonardo.Venceslau@rfsmart.com>
2024-07-02 10:20:17 -0400

    update: lsp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -100,7 +100,6 @@ return {
 					vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = 0 })
 
 					vim.keymap.set("n", "<space>cr", vim.lsp.buf.rename, { buffer = 0 })
-					vim.keymap.set("n", "<space>ca", vim.lsp.buf.code_action, { buffer = 0 })
 
 					local filetype = vim.bo[bufnr].filetype
 					if disable_semantic_tokens[filetype] then
@@ -118,6 +117,38 @@ return {
 					end
 				end,
 			})
+
+			local icons = require("config.icons")
+
+			local default_diagnostic_config = {
+				signs = {
+					active = true,
+					values = {
+						{ name = "DiagnosticSignError", text = icons.diagnostics.Error },
+						{ name = "DiagnosticSignWarn", text = icons.diagnostics.Warning },
+						{ name = "DiagnosticSignHint", text = icons.diagnostics.Hint },
+						{ name = "DiagnosticSignInfo", text = icons.diagnostics.Information },
+					},
+				},
+				virtual_text = false,
+				update_in_insert = false,
+				underline = true,
+				severity_sort = true,
+				float = {
+					focusable = true,
+					style = "minimal",
+					border = "rounded",
+					source = "always",
+					header = "",
+					prefix = "",
+				},
+			}
+
+			vim.diagnostic.config(default_diagnostic_config)
+
+			for _, sign in ipairs(vim.tbl_get(vim.diagnostic.config(), "signs", "values") or {}) do
+				vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = sign.name })
+			end
 
 			require("conform").setup({
 				formatters_by_ft = {
