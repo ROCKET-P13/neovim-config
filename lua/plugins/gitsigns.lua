5d5dce6026fe7694f3a8cab4a9a87bd3f6b13165
Leo <135278016+ROCKET-P13@users.noreply.github.com>
2025-08-14 08:18:32 -0400

    feat: jump between git changes with gitsigns

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,5 +1,35 @@
 local M = {
 	"lewis6991/gitsigns.nvim",
+	config = function()
+		require("gitsigns").setup({
+			on_attach = function(bufnr)
+				local gs = package.loaded.gitsigns
+				vim.keymap.set("n", "]c", function()
+					if vim.wo.diff then
+						return "]c"
+					end
+					vim.schedule(function()
+						gs.next_hunk()
+					end)
+					return "<Ignore>"
+				end, { expr = true, buffer = bufnr })
+
+				vim.keymap.set("n", "[c", function()
+					if vim.wo.diff then
+						return "[c"
+					end
+					vim.schedule(function()
+						gs.prev_hunk()
+					end)
+					return "<Ignore>"
+				end, { expr = true, buffer = bufnr })
+
+				vim.keymap.set("n", "<leader>hs", gs.stage_hunk)
+				vim.keymap.set("n", "<leader>hr", gs.reset_hunk)
+				vim.keymap.set("n", "<leader>hp", gs.preview_hunk)
+			end,
+		})
+	end,
 }
 
 return M
