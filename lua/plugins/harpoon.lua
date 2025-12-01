a5a9ebd2c5fdaa8de2ef2c909a97ab0ca11c2ea9
ROCKET-P13 <135278016+ROCKET-P13@users.noreply.github.com>
2025-12-01 06:36:03 -0500

    update: harpoon

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -4,7 +4,26 @@ local M = {
 		"nvim-lua/plenary.nvim",
 	},
 	config = function()
-		local harpoon = require("harpoon")
+		vim.keymap.set("n", "<leader>ha", function()
+			require("harpoon.mark").add_file()
+		end)
+
+		vim.keymap.set("n", "<C-e>", function()
+			require("harpoon.ui").toggle_quick_menu()
+		end)
+
+		vim.keymap.set("n", "<leader>1", function()
+			require("harpoon.ui").nav_file(1)
+		end)
+		vim.keymap.set("n", "<leader>2", function()
+			require("harpoon.ui").nav_file(2)
+		end)
+		vim.keymap.set("n", "<leader>3", function()
+			require("harpoon.ui").nav_file(3)
+		end)
+		vim.keymap.set("n", "<leader>4", function()
+			require("harpoon.ui").nav_file(4)
+		end)
 	end,
 }
 
