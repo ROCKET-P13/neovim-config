83516e869d3faaff6ba56af57396c274d1776846
ROCKET-P13 <135278016+ROCKET-P13@users.noreply.github.com>
2025-12-02 07:58:41 -0500

    update: snippets + more things

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -1,10 +1,31 @@
 local M = {
 	"saghen/blink.cmp",
 	version = "1.*",
+	dependencies = {
+		{
+			"L3MON4D3/LuaSnip",
+			version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+			build = "make install_jsregexp",
+		},
+	},
 	config = function()
+		require("luasnip.loaders.from_vscode").lazy_load({
+			paths = { vim.fn.stdpath("config") .. "/snippets" },
+		})
+
 		require("blink.cmp").setup({
 			keymap = {
 				["<CR>"] = { "select_and_accept", "fallback" },
+				["<Tab>"] = {
+					"select_next",
+					"snippet_forward",
+					"fallback",
+				},
+				["<S-Tab>"] = {
+					"select_prev",
+					"snippet_backward",
+					"fallback",
+				},
 			},
 			enabled = function()
 				local node = vim.treesitter.get_node()
@@ -16,6 +37,10 @@ local M = {
 
 				return not disabled
 			end,
+			snippets = { preset = "luasnip" },
+			sources = {
+				default = { "lsp", "snippets", "buffer" },
+			},
 			completion = {
 				accept = {
 					auto_brackets = {
