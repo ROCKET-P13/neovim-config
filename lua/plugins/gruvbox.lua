e3ee6483b7a5c277302b7591407aaa0023b93ee9
ROCKET-P13 <135278016+ROCKET-P13@users.noreply.github.com>
2026-02-20 10:02:47 -0500

    update: tag_delimiter highlighting update

--- lua/plugins/gruvbox.lua
+++ lua/plugins/gruvbox.lua
@@ -129,6 +129,7 @@ local M = {
 		vim.api.nvim_set_hl(0, "@tag.builtin.javascript", { link = "GruvboxAqua" })
 		vim.api.nvim_set_hl(0, "@jsx.attr.name", { link = "GruvboxYellow" })
 		vim.api.nvim_set_hl(0, "@jsx.tag.delimiter", { link = "GruvboxGray" })
+		vim.api.nvim_set_hl(0, "@tag.delimiter.javascript", { link = "GruvboxGray" })
 
 		-- typescript highlights
 		vim.api.nvim_set_hl(0, "@punctuation.special.typescript", { link = "GruvboxGray" })
