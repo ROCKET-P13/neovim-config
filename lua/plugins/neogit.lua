00591d521f14eaa88553ca1d59aba10afea87116
ROCKET-P13 <leonardocvenceslau@gmail.com>
2026-05-28 10:08:13 -0400

    feat: neogit

---
+++ lua/plugins/neogit.lua
@@ -0,0 +1,20 @@
+return {
+	"NeogitOrg/neogit",
+	lazy = true,
+	dependencies = {
+		"esmuellert/codediff.nvim",
+	},
+	cmd = "Neogit",
+	keys = {
+		{ "<leader>gg", "<cmd>Neogit<cr>", desc = "Show Neogit UI" },
+	},
+	config = function()
+		require("neogit").setup({
+			disable_line_numbers = false,
+			integrations = {
+				codediff = true,
+			},
+			diff_viewer = "codediff",
+		})
+	end,
+}
